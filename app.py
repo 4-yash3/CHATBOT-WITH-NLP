8b9cab0651c948cc6f761de614c666faa9c21cc4
4-yash3 <167114861+4-yash3@users.noreply.github.com>
2024-07-22 11:16:37 +0530

    website for the chatbots

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='D:/RASA/src/templates')
+
 
 @app.route('/')
 def home():
-    return render_template('D:\RASA\CHATBOT-WITH-NLP\Tempelate\index.html')
+    return render_template('index.html')
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    user_message = request.json.get("message")
-    response = requests.post('http://localhost:5005/webhooks/rest/webhook', json={"sender": "user", "message": user_message})
-    response_text = response.json()
-    if response_text:
-        return jsonify({"response": response_text[0]["text"]})
-    return jsonify({"response": "Sorry, I didn't get that. Could you please rephrase?"})
+    user_message = request.json['message']
+    rasa_url = "http://localhost:5005/webhooks/rest/webhook"
+    payload = {
+        "sender": "user",
+        "message": user_message
+    }
+    response = requests.post(rasa_url, json=payload)
+    response_data = response.json()
+    if response_data:
+        bot_response = response_data[0]['text']
+    else:
+        bot_response = "Sorry, I didn't get that. Could you please rephrase?"
+    return jsonify({"response": bot_response})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
