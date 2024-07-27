1729e0aeefb2bb6fbd80397153a96308d8fbdc55
4-yash3 <167114861+4-yash3@users.noreply.github.com>
2024-07-27 17:11:50 +0530

    successfull creation of web app

--- src/actions/actions.py
+++ src/actions/actions.py
@@ -1,20 +1,2 @@
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
 
-class ActionGetWeather(Action):
-
-    def name(self) -> Text:
-        return "action_get_weather"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        location = tracker.get_slot('location')
-        weather = "sunny"  # Here you would integrate your weather API
-        response = f"The weather in {location} is currently {weather}."
-
-        dispatcher.utter_message(text=response)
-        return []
 
